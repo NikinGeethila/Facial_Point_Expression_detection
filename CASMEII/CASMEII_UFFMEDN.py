d4ff2003d415bf686c09cd14d8d17a4fd2078979
Nikin Geethila Matharaarachchi <54057518+NikinGeethila@users.noreply.github.com>
2020-08-11 02:32:13 +0800

    Update CASMEII_UFFMEDN.py

--- CASMEII/CASMEII_UFFMEDN.py
+++ CASMEII/CASMEII_UFFMEDN.py
@@ -65,7 +65,7 @@ def evaluate(segment_train_images, segment_validation_images, segment_train_labe
 
     # Training the model
 
-    history = model.fit(segment_train_images, segment_train_labels, validation_data = (segment_validation_images, segment_validation_labels), callbacks=callbacks_list, batch_size = 16, nb_epoch = 500, shuffle=True,verbose=1)
+    history = model.fit(segment_train_images, segment_train_labels, validation_data = (segment_validation_images, segment_validation_labels), callbacks=callbacks_list, batch_size = 8, nb_epoch = 500, shuffle=True,verbose=1)
 
 
 
